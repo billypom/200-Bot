bc4a05d7bd44dc0fd56fa22221b41f02abf08cd6
billy pompey <billypompey@icloud.com>
2022-07-26 00:18:49 -0400

    a

--- lounge.py
+++ lounge.py
@@ -856,7 +856,7 @@ async def create_teams(ctx, poll_results):
         team_mmr = temp_mmr/len(team)
         team.append(team_mmr)
 
-    sorted_list = reverse(sorted(chunked_list, key = lambda x: int(x[len(chunked_list[0])-1])))
+    sorted_list = sorted(chunked_list, key = lambda x: int(x[len(chunked_list[0])-1])).reverse()
     print(sorted_list)
     
     for idx, team in enumerate(sorted_list):
