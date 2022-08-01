d7fa5db7f7c52efd881494c8d4f23b537da368d5
billy pompey <billypompey@icloud.com>
2022-08-01 18:46:26 -0400

    a

--- lounge.py
+++ lounge.py
@@ -886,9 +886,9 @@ async def stats(
     tier: discord.Option(discord.TextChannel, description='Choose a channel', required=False)
     ):
     await ctx.defer()
-    print(tier.mention)
+    print(tier.id)
     return
-    if tier.mention in TIER_ID_LIST:
+    if tier.id in TIER_ID_LIST:
         print('yay')
         await ctx.respond('yay!')
     else:
