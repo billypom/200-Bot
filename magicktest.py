30006f3343fe6b1d72ec65584506e5bb3dbf36bc
Billy Pompey <billy@Billys-MacBook-Pro.local>
2022-08-02 13:01:15 -0400

    test imagemagick

--- magicktest.py
+++ magicktest.py
@@ -2,4 +2,4 @@ import os
 import subprocess
 
 mmr_table_string = '<span foreground="chartreuse">heyy</span>'
-correct = subprocess.run(['convert', '-background', 'gray21', 'pango:', '<tt><span foreground="white">{mmr_table_string}</span></tt>', f'testmmr.png'], check=True, text=True)
+correct = subprocess.run(['convert', '-background', 'gray21', 'pango:<tt><span foreground="white">{mmr_table_string}</span></tt>', f'testmmr.png'], check=True, text=True)
