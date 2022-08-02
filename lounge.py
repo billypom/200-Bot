cd2b570a57503443031af7eefa8e0f0c9db44e2d
Billy Pompey <billy@Billys-MacBook-Pro.local>
2022-08-02 10:42:36 -0400

    stats when 0 matches

--- lounge.py
+++ lounge.py
@@ -965,7 +965,7 @@ async def stats(
     if tier is None:
         with DBA.DBAccess() as db:
             temp = db.query('SELECT mmr_change, score FROM player_mogi pm JOIN mogi m ON pm.mogi_id = m.mogi_id WHERE player_id = %s ORDER BY m.create_date ASC;', (ctx.author.id,))
-            if temp[0][0] is None: # Haven't played any matches
+            if temp is None: # Haven't played any matches
                 await ctx.respond('You must play at least 1 match to use `/stats`')
                 return
             for i in range(len(temp)):
