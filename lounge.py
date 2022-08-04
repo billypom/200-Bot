3398fd0be504922962b9bf61b070f06043f089a5
billy pompey <billypompey@icloud.com>
2022-08-04 09:08:17 -0400

    table with headers

--- lounge.py
+++ lounge.py
@@ -862,7 +862,7 @@ async def table(
             string_mogi_format = f'{str(mogi_format)}v{str(mogi_format)}'
 
         mmr_table_string = f'<big><big>{ctx.channel.name} {string_mogi_format}</big></big>\n'
-        mmr_table_string += f'place |       name       |  mmr  |  +/-  | new mmr |  rankups'
+        mmr_table_string += f'PLACE |       NAME       |  MMR  |  +/-  | NEW MMR |  RANKUPS'
 
         for team in sorted_list:
             # print(team)
