2a5de2e482fe479ee981d630eba8981975d97980
billy pompey <billypompey@icloud.com>
2022-08-04 11:26:40 -0400

    start_mogi -> create_teams fix for /table command

--- lounge.py
+++ lounge.py
@@ -998,11 +998,11 @@ async def table(
                         db.execute('INSERT INTO player_mogi (player_id, mogi_id, place, score, prev_mmr, mmr_change, new_mmr, is_sub) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);', (player[0], db_mogi_id, int(my_player_place), int(my_player_score), int(my_player_mmr), int(my_player_mmr_change), int(my_player_new_mmr), is_sub))
                         print('c')
                         # Update player record
-                        db.execute('UPDATE player SET mmr = %s, peak_mmr = %s WHERE player_id = %s;', (my_player_new_mmr, my_player_new_mmr, player[0]))
+                        db.execute('UPDATE player SET mmr = %s WHERE player_id = %s;', (my_player_new_mmr, player[0]))
                         # Remove player from lineups
                         # db.execute('DELETE FROM lineups WHERE player_id = %s AND tier_id = %s;', (player[0], ctx.channel.id)) # YOU MUST SUBMIT TABLE IN THE TIER THE MATCH WAS PLAYED
                 except Exception as e:
-                    # print('duplicate player...skipping')
+                    print(e)
                     pass
 
                 # Check for rank changes
