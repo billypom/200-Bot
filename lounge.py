8edb4c5543caa6e9d9f96af5263d6e09fed0f294
billy pompey <billypompey@icloud.com>
2022-08-16 13:59:45 -0400

    a

--- lounge.py
+++ lounge.py
@@ -2074,7 +2074,7 @@ async def get_partner_avg(uid, *mogi_format):
         with DBA.DBAccess() as db:
             temp = db.query('SELECT AVG(score) FROM (SELECT player_id, mogi_id, place, score FROM player_mogi WHERE player_id <> %s AND (mogi_id, place) IN (SELECT mogi_id, place FROM player_mogi WHERE player_id = %s)) as table2;', (uid, uid))
             try:
-                return round(float(temp[0][0]), 0)
+                return round(float(temp[0][0]), 2)
             except Exception as e:
                 return 0
     except Exception as e:
