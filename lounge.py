544228e971b8d5aecb33fb68c1f2a0eebd2787e0
billy pompey <billypompey@icloud.com>
2022-08-27 10:42:27 -0400

    derived table name bugfix

--- lounge.py
+++ lounge.py
@@ -2517,7 +2517,7 @@ async def check_if_uid_in_specific_tier(uid, tier):
 async def check_if_uid_in_first_12_of_tier(uid, tier):
     try:
         with DBA.DBAccess() as db:
-            temp = db.query('SELECT player_id FROM (SELECT player_id FROM lineups WHERE tier_id = %s) WHERE player_id = %s;', (tier, uid))
+            temp = db.query('SELECT player_id FROM (SELECT player_id FROM lineups WHERE tier_id = %s) as derived_table WHERE player_id = %s;', (tier, uid))
             if temp[0][0] == uid:
                 return True
             else:
