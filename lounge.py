c50594a359ebebe8da06c7423a0533c4f9f1772c
billy pompey <billypompey@icloud.com>
2022-10-19 17:48:01 -0400

    a

--- lounge.py
+++ lounge.py
@@ -3659,6 +3659,6 @@ async def peak_mmr_wrapper(input):
     # return (f'[0;2m[0;41m[0;37m{input}[0m[0;41m[0m[0m')
     return (f'<span foreground="Yellow1"><i>{input}</i></span>')
 
-client.add_cog(MyCog)
+# client.add_cog(MyCog)
 
 client.run(secretly.token)
