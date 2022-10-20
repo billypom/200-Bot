a7a988b720c950e3f3e1049db51bff8cbcaf0f72
billy pompey <billypompey@icloud.com>
2022-10-20 00:13:39 -0400

    a

--- cogs/inactivity_check.py
+++ cogs/inactivity_check.py
@@ -11,14 +11,6 @@ class inactivity_check(commands.Cog):
         self.check.start()
         self.client = client
 
-    # def cog_unload(self):
-    #     self.printer.cancel()
-
-    # @tasks.loop(seconds=5)
-    # async def printer(self):
-    #     print(self.index)
-    #     self.index +=1
-
     async def send_raw_to_debug_channel(self, anything, error):
         channel = self.client.get_channel(secretly.debug_channel)
         embed = discord.Embed(title='Error', description='>.<', color = discord.Color.yellow())
@@ -34,11 +26,12 @@ class inactivity_check(commands.Cog):
         print(f'checking inactivity | {secretly.debug_channel}')
         await self.send_raw_to_debug_channel(f'`:` Checking inactivity...', 'Test')
         unix_now = time.mktime(datetime.datetime.now().timetuple())
+        print(unix_now)
         try:
             with DBA.DBAccess() as db:
                 temp = db.query('SELECT l.player_id, UNIX_TIMESTAMP(l.last_active), l.tier_id, l.wait_for_activity, p.player_name FROM lineups as l JOIN player as p ON l.player_id = p.player_id WHERE l.can_drop = %s;', (1,))
         except Exception as e:
-            await send_raw_to_debug_channel(f'inactivity_check error 1 {secrely.my_discord}', e)
+            await self.send_raw_to_debug_channel(f'inactivity_check error 1 {secrely.my_discord}', e)
             return
         for i in range(len(temp)):
             name = temp[i][4]
@@ -46,7 +39,7 @@ class inactivity_check(commands.Cog):
             # print(f'{unix_now} - {temp[i][1]} = {unix_difference}')
             if unix_difference < 900: # if it has been less than 15 minutes
                 if unix_difference > 600: # if it has been more than 10 minutes
-                    channel = client.get_channel(temp[i][2])
+                    channel = self.client.get_channel(temp[i][2])
                     if temp[i][3] == 0: # false we are not waiting for activity
                         message = f'<@{temp[i][0]}> Type anything in the chat in the next 5 minutes to keep your spot in the mogi.'
                         await channel.send(message, delete_after=300)
@@ -55,7 +48,7 @@ class inactivity_check(commands.Cog):
                             with DBA.DBAccess() as db:
                                 db.execute('UPDATE lineups SET wait_for_activity = %s WHERE player_id = %s;', (1, temp[i][0])) # we are waiting for activity
                         except Exception as e:
-                            await send_raw_to_debug_channel(f'inactivity_check error 2 {secrely.my_discord}', e)
+                            await self.send_raw_to_debug_channel(f'inactivity_check error 2 {secrely.my_discord}', e)
                             return
                 else: # has not been at least 10 minutes yet
                     continue # does this make it faster? idk
@@ -65,10 +58,10 @@ class inactivity_check(commands.Cog):
                     with DBA.DBAccess() as db:
                         db.execute('DELETE FROM lineups WHERE player_id = %s;', (temp[i][0],))
                 except Exception as e:
-                    await send_raw_to_debug_channel(f'{secrely.my_discord} inactivity_check - cannot delete from lineup',f'{temp[i][0]} | {temp[i][1]} | {temp[i][2]} | {e}')
+                    await self.send_raw_to_debug_channel(f'{secrely.my_discord} inactivity_check - cannot delete from lineup',f'{temp[i][0]} | {temp[i][1]} | {temp[i][2]} | {e}')
                     return
                 # Send message
-                channel = client.get_channel(temp[i][2])
+                channel = self.client.get_channel(temp[i][2])
                 message = f'{name} has been removed from the mogi due to inactivity'
                 await channel.send(message)
             else:
