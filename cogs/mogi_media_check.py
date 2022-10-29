e985c8352f1b964fd8f6e89e616623474da14636
billy pompey <billypompey@icloud.com>
2022-10-29 11:53:30 -0400

    remove future result from cogs

--- cogs/mogi_media_check.py
+++ cogs/mogi_media_check.py
@@ -68,14 +68,14 @@ class mogi_media_check(commands.Cog):
 
     @tasks.loop(seconds=30)
     async def mogi_media(self):
-        print('checking mogi media...')
+        # print('checking mogi media...')
         try:
             with DBA.DBAccess() as db:
                 temp = db.query('SELECT p.twitch_link, p.mogi_media_message_id, p.player_id FROM player p JOIN lineups l ON p.player_id = l.player_id WHERE l.can_drop = 0;', ())
         except Exception:
             return
         streams = await self.get_live_streamers(temp)
-        print(streams)
+        # print(streams)
 
         # print(f'future.result from thread executor: {streams}')
         for stream in streams:
@@ -94,8 +94,8 @@ class mogi_media_check(commands.Cog):
                 # If not live
                 else:
                     if stream[4] > 0: 
-                        member_future = await GUILD.fetch_member(stream[5])
-                        member = member_future.result()               
+                        member = await GUILD.fetch_member(stream[5])
+                        # member = member_future.result()
                         channel = self.client.get_channel(self.mogi_media_channel_id)
                         message = await channel.fetch_message(stream[4])
                         await message.delete()
