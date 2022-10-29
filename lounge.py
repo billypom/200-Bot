6f59fd32731b564633f4658dbc579b5c6d0d0e99
billy pompey <billypompey@icloud.com>
2022-10-29 14:15:32 -0400

    /esn delete players

--- lounge.py
+++ lounge.py
@@ -895,12 +895,12 @@ async def esn(ctx):
         return
     try:
         with DBA.DBAccess() as db:
-            players = db.query('SELECT player_id FROM lineups WHERE tier_id = %s AND mogi_start_time <> NULL;', (ctx.channel.id,))
+            players = db.query('SELECT player_id FROM lineups WHERE tier_id = %s AND mogi_start_time IS NOT NULL;', (ctx.channel.id,))
         print(players)
         for player in players:
             with DBA.DBAccess() as db:
                 db.execute('DELETE FROM lineups WHERE player_id = %s AND tier_id = %s;', (player[0], ctx.channel.id))
-        await ctx.respond('`New mogi started')
+        await ctx.respond('New mogi started')
     except Exception as e:
         await send_to_debug_channel(ctx, f'esn error 2: {e}')
         await ctx.respond('`Error 63:` esners')
