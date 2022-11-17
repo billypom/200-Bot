1ee953c031d236f7e12f712e13b666d4708a8bbc
billy pompey <billypompey@icloud.com>
2022-11-16 23:17:27 -0500

    suggest live

--- lounge.py
+++ lounge.py
@@ -2968,7 +2968,7 @@ async def send_to_suggestion_log_channel(ctx, suggestion_id, message, decision, 
     else:
         embed = discord.Embed(title=f'Suggestion #{suggestion_id} Denied', description=f'', color=discord.Color.red())
     embed.set_author(name=member.display_name, icon_url=member.avatar.url)
-    embed.add_field(name='Suggestion', value=message, inline=False)
+    embed.add_field(name='----------', value=message, inline=False)
     embed.add_field(name=f'Reason from {ctx.author.display_name}', value=reason)
     x = await channel.send(content=None, embed=embed)
     return x
