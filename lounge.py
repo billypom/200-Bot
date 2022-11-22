ccfe30441150897d06c4254843d2458add540979
billy pompey <billypompey@icloud.com>
2022-11-22 15:45:02 -0500

    /votes command live

--- lounge.py
+++ lounge.py
@@ -665,7 +665,43 @@ async def esn(ctx):
     #     await send_to_debug_channel(ctx, f'Cancel Error Deletion:{e}')
     #     return
 
-
+@client.slash_command(
+    name='votes',
+    description='See the current votes',
+    guild_ids=Lounge
+)
+async def votes(ctx):
+    await ctx.defer()
+    is_ongoing = await check_if_mogi_is_ongoing(ctx)
+    if is_ongoing:
+        pass
+    else:
+        await ctx.respond('The mogi has not started.')
+        return
+    vote_dict = {}
+    return_string = ""
+    remove_chars = {
+        39:None, # '
+        91:None, # [
+        93:None, # ]
+    }
+    try:
+        with DBA.DBAccess() as db:
+            temp = db.query('SELECT p.player_name, l.vote FROM player as p JOIN lineups as l ON p.player_id = l.player_id WHERE l.tier_id = %s ORDER BY l.create_date ASC LIMIT %s;', (ctx.channel.id, MAX_PLAYERS_IN_MOGI))
+        # print(f'temp: {temp}')
+        vote_dict = {1:[],2:[],3:[],4:[],6:[]}
+        for player in temp:
+            # print(player)
+            if player[1] in vote_dict:
+                vote_dict[player[1]].append(player[0])
+            else:
+                vote_dict[player[1]] = [player[0]]
+        return_string = f'`FFA:` {vote_dict[1]}\n`2v2:` {vote_dict[2]}\n`3v3:` {vote_dict[3]}\n`4v4:` {vote_dict[4]}\n`6v6:` {vote_dict[6]}'
+        return_string = return_string.translate(remove_chars)
+    except Exception as e:
+        await send_to_debug_channel(ctx, f'/votes | {e}')
+        await ctx.respond('`Error 64:` Could not retrieve the votes')
+    await ctx.respond(return_string)
 
 # /fc
 @client.slash_command(
