f1632f207a9d8f8503ccf8d2605338bd2db6276a
Billy Pom <billypompey@icloud.com>
2023-01-01 12:53:12 -0500

    200lounge.csv same directory

--- lounge.py
+++ lounge.py
@@ -2598,7 +2598,7 @@ async def create_player(member, mkc_user_id, country_code):
             insert_name = temp_name
         altered_name = str(insert_name).replace(" ", "-")
         
-        f = open('./200lounge.csv',encoding='utf-8-sig') # f is our filename as string
+        f = open('200lounge.csv',encoding='utf-8-sig') # f is our filename as string
         lines = list(csv.reader(f,delimiter=',')) # lines contains all of the rows from the csv
         f.close()
         for line in lines:
