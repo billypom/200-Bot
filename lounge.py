71f990ef026b593a7354943de19a4eca99a97303
Billy Pom <billypompey@icloud.com>
2023-06-10 18:27:37 -0400

    faster assigning

--- lounge.py
+++ lounge.py
@@ -3343,7 +3343,7 @@ async def zassign_ranks2(ctx):
         temp = db.query('SELECT rank_id, mmr_min, mmr_max FROM ranks', ())
     for i in range(len(players)):
         if players[i][1] is None:
-            role = guild.get_role(PLACEMENT_ROLE_ID)
+            role = GUILD.get_role(PLACEMENT_ROLE_ID)
             await member.add_roles(role)
             print(f'assigned {role} to {players[i][0]}')
             continue
