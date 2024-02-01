16139dd4a05d4ae16b87ee354bf2e490a5c09edb
billypom on debian <billypompey@icloud.com>
2024-02-01 17:39:41 -0500

    bug fixes

--- cogs/zloungeless.py
+++ cogs/zloungeless.py
@@ -47,7 +47,10 @@ class LoungelessCog(commands.Cog):
             unban_date = unix_now + unix_ban_length
             await user.add_roles(loungeless_role)
             await remove_rank_roles_from_uid(self.client, player_id)
-            await user.send(f'You have been banned from competing in MK8DX 200cc Lounge.\nBan length: {ban_length} days\nReason: `{reason}`')
+            try:
+                await user.send(f'You have been banned from competing in MK8DX 200cc Lounge.\nBan length: {ban_length} days\nReason: `{reason}`')
+            except Exception as e:
+                await send_raw_to_debug_channel(self.client, '/zloungeless error - Failed to send user DM. Probably not allowed to because they are too awesome :)')
             try:
                 with DBA.DBAccess() as db:
                     db.execute('INSERT INTO player_punishment (player_id, punishment_id, reason, admin_id, unban_date) VALUES (%s, %s, %s, %s, %s);', (player_id, 2, reason, ctx.author.id, unban_date))
