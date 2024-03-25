9ed20397eb69eec8e823401736894cb2ff7fa81c
billypom on debian <billypompey@icloud.com>
2024-03-24 22:43:13 -0400

    results channel to tier channel transformation or something

---
+++ helpers/getters/get_results_id_list.py
@@ -0,0 +1,11 @@
+import DBA
+
+async def get_results_id_list() -> list:
+    """Returns a list of all result_id from DB tier table"""
+    # Used to make sure certain actions only happen in the tiers
+    results = list() 
+    with DBA.DBAccess() as db:
+        result = db.query('SELECT results_id FROM tier WHERE tier_id > %s;', (0,))
+        for i in range(len(result)):
+            results.append(result[i][0])
+    return results
